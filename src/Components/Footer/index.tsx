bb635436f96375efae06c1a04e399846567dba5e
Oghenetega Esedere <arcteggzz@gmail.com>
2024-01-18 21:49:25 +0100

    feat: implemented Footer

--- src/Components/Footer/index.tsx
+++ src/Components/Footer/index.tsx
@@ -1,10 +1,95 @@
 import styles from "./Footer.module.scss";
+import { Link } from "react-router-dom";
+import { routePaths, socialLinks1, socialLinks2 } from "../../utils";
+import ojoviz_logo from "../../assets/images/ojoviz_logo.png";
 
 const Footer = () => {
+  const getYear = () => {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    return `© ${currentYear} ojoviz. All rights reserved.`;
+  };
+
   return (
     <>
       <footer className={styles.Footer}>
-        <h1>Footer P</h1>
+        <div className={styles.top_footer}>
+          <div className={styles.slogan_container}>
+            <Link to={routePaths.HOME} className={styles.logo_icon}>
+              <img src={ojoviz_logo} alt="Ojoviz home logo" />
+            </Link>
+            <p className={styles.slogan_text}>
+              We listen, understand and churn out the best visuals
+            </p>
+          </div>
+
+          <div className={styles.links_and_socials}>
+            <div className={styles.footer_links}>
+              <Link
+                to={`${routePaths.HOME}#aboutUs`}
+                className={styles.single_footer_Link}
+              >
+                About Us
+              </Link>
+              <Link
+                to={routePaths.PROJECTS}
+                className={styles.single_footer_Link}
+              >
+                Projects
+              </Link>
+              <Link
+                to={`${routePaths.HOME}#contactUs`}
+                className={styles.single_footer_Link}
+              >
+                Contact Us
+              </Link>
+            </div>
+
+            <div className={styles.socials_container}>
+              {socialLinks1.map((item) => {
+                return (
+                  <Link
+                    to={item.externalLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className={styles.single_social_link}
+                  >
+                    <img
+                      src={item.image}
+                      alt={item.alt}
+                      className={styles.socials_icon}
+                    />
+                  </Link>
+                );
+              })}
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.bottom_footer}>
+          <p className={styles.restricted_text}>{getYear()}</p>
+
+          <div className={styles.external_links}>
+            {socialLinks2.map((item) => {
+              return (
+                <Link
+                  to={item.externalLink}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className={styles.single_external_social_link}
+                >
+                  <img
+                    src={item.image}
+                    alt={item.alt}
+                    className={styles.socials_icon}
+                  />
+
+                  <p className={styles.external_link_text}>{item.text}</p>
+                </Link>
+              );
+            })}
+          </div>
+        </div>
       </footer>
     </>
   );
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,8 +2,16 @@ export { default as PublicPageLayout } from "./PublicPageLayout";
 export { default as AnimatedFadeInPage } from "./AnimatedFadeInPage";
 export { default as routePaths } from "./routePaths";
 export { projectsList } from "./projectsList";
+import website_logo from "../assets/images/website_logo.png";
+import contact_logo from "../assets/images/contact_logo.png";
+// import facebook_logo from "../assets/images/facebook_logo.png";
+import instagram_logo from "../assets/images/instagram_logo.png";
+import linkedin_logo from "../assets/images/linkedin_logo.png";
+import mail_logo from "../assets/images/mail_logo.png";
+import twitter_logo from "../assets/images/twitter_logo.png";
 
-export const emailAddress = "arcteggzz@gmail.com";
+export const emailAddress = "jordanomoleye@gmail.com";
+export const phoneNumber = "+2348178296321";
 
 export const sendEmail = () => {
   const mailtoLink = "mailto:" + emailAddress;
@@ -27,5 +35,51 @@ export const aboutUsItems = [
   {
     head: "6000+",
     body: "Hours worked",
+  },
+];
+
+export const socialLinks1 = [
+  // {
+  //   image: facebook_logo,
+  //   alt: "Facebook Icon",
+  //   externalLink: "",
+  // },
+  {
+    image: twitter_logo,
+    alt: "Twitter Icon",
+    externalLink: "https://x.com/jordan_rvt?s=11",
+  },
+  {
+    image: instagram_logo,
+    alt: "Instagram Icon",
+    externalLink:
+      "https://www.instagram.com/ojoviz?igsh=MW94a3VwMTBwdWxjeA%3D%3D&utm_source=qr",
+  },
+  {
+    image: linkedin_logo,
+    alt: "Linkedin Icon",
+    externalLink:
+      "https://www.linkedin.com/in/jordanomoleye?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+  },
+];
+
+export const socialLinks2 = [
+  {
+    image: website_logo,
+    alt: "Website Icon",
+    externalLink: "https://x.com/jordan_rvt?s=11",
+    text: "Jordan.rvt",
+  },
+  {
+    image: contact_logo,
+    alt: "Contact Icon",
+    externalLink: "tel" + phoneNumber,
+    text: "+2348178296321",
+  },
+  {
+    image: mail_logo,
+    alt: "Mail Icon",
+    externalLink: "mailto:" + emailAddress,
+    text: "Jordan_leye@outlook.com",
   },
 ];
